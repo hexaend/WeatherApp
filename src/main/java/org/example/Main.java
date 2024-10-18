60a60702e4f4e7266ae3af32e40bd33d253e8765
user <kek@kek>
2024-10-18 14:18:21 +0300

    added loop and exit command

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,31 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Scanner;
 
-import static org.example.api.ApiMain.*;
+import static org.example.api.ApiMain.getWeatherOnTomorrow;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.printf("Добро пожаловать в консольное приложение - Погода на завтра%nВведите город: ");
-        String city = new Scanner(System.in).next();
-        System.out.println(getWeatherOnTomorrow(city));
+        System.out.printf("Добро пожаловать в консольное приложение - Погода на завтра%n");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите город: ");
+            try {
+                String city = scanner.next();
+                if (Objects.equals(city, "exit")) {
+                    System.out.println("До свидания.");
+                    System.exit(0);
+                }
+                System.out.println(getWeatherOnTomorrow(city));
+            } catch (NoSuchElementException elementException) {
+                System.out.println("До свидания.");
+                break;
+            }
+
+
+        }
     }
 }
