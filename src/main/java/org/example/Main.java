fed9d41c531fb5c7ca9c1e1846b5aa06edd3ffa3
user <kek@kek>
2024-10-19 19:15:00 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,27 @@
 package org.example;
 
-import java.io.IOException;
-import java.util.NoSuchElementException;
+import java.io.*;
 import java.util.Objects;
-import java.util.Scanner;
 
 import static org.example.api.ApiMain.getWeatherOnTomorrow;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.printf("Добро пожаловать в консольное приложение - Погода на завтра%n");
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
         while (true) {
+            Console console = System.console();
             System.out.print("Введите город: ");
-            try {
-                String city = scanner.next();
-                if (Objects.equals(city, "exit")) {
-                    System.out.println("До свидания.");
-                    System.exit(0);
-                }
-                System.out.println(getWeatherOnTomorrow(city));
-            } catch (NoSuchElementException elementException) {
+            String city = console.readLine();
+            if (Objects.equals(city, null) || city.trim().isEmpty() || Objects.equals(city, "exit")) {
                 System.out.println("До свидания.");
-                break;
+                System.exit(0);
             }
+            System.out.println(city);
+            System.out.println(getWeatherOnTomorrow(city));
 
 
         }
     }
+
 }
